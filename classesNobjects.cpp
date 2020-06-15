8c28e4ecf2e204a5029d2c84bc9bddb60f2291e0
Jason <jvnhong@ucdavis.edu>
2020-06-14 19:33:08 -0700

    first commit

---
+++ classesNobjects.cpp
@@ -0,0 +1,28 @@
+// when trying to call a function in class, ALWAYS have to create an object first... 
+// good practice to use setters and getters in public 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BuckysClass
+{
+    public: // good practice to create private functions and add objects through public 
+        void setName(string x)
+        {
+            name = x;
+        }
+        string getName()
+        {
+            return name;
+        }
+    private:
+        string name;
+};
+
+int main()
+{
+    BuckysClass test; 
+    test.setName("Jason");
+    cout << test.getName() << endl;
+    return 0;
+}
