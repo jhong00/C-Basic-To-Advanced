ef30f51be9595ff8cf851a04ba9e20244d77500d
Jason <jvnhong@ucdavis.edu>
2020-06-17 21:58:58 -0700

    12th

---
+++ example12.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void passByValue(int x);
+void passByReference(int *x);
+
+int main()
+{
+    int betty = 13;
+    int sandy = 13;
+
+    passByValue(betty);
+    passByReference(&sandy); // & (memory address) is necessary for pointers 
+
+    cout << "betty is now " << betty << endl;
+    cout << "sandy is now " << sandy << endl; 
+
+}
+
+void passByValue(int x)
+{
+    x = 10;
+}
+
+void passByReference(int *x)
+{
+    *x = 20; // passes to sandy b/c pointer 
+}
